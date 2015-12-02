bbc4b8af3c4460f196be9043f81593147cb5c736
samir04m <samir4mt@gmail.com>
2015-12-02 09:32:28 -0500

    Se agrego la activity de nacegacion y se cambio las opciones y la imagen

--- app/src/main/java/co/edu/unimagdalena/mellanie/MainActivity.java
+++ app/src/main/java/co/edu/unimagdalena/mellanie/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -22,9 +24,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FacebookSdk.sdkInitialize(getApplicationContext()); //inicializando el sdk de facebook
+
         setContentView(R.layout.activity_main);
+
+        if (estaLoguedo()){
+            Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
+
+            startActivity(intent);
+        }
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
         titulolbl = (TextView) findViewById(R.id.txtLoginPage);
@@ -35,18 +43,23 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                titulolbl.setText(loginResult.getAccessToken().getUserId()); //cambia el texto por la id del ususario
+                Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
+
+                startActivity(intent);
+                //titulolbl.setText(loginResult.getAccessToken().getUserId()); //cambia el texto por la id del ususario
             }
 
             @Override
             public void onCancel() {
-                titulolbl.setText("User Cancel reques");
+                Toast.makeText(getApplicationContext(), "Cancelaste el Login", Toast.LENGTH_LONG);
+                //titulolbl.setText("User Cancel reques");
 
             }
 
             @Override
             public void onError(FacebookException error) {
-                titulolbl.setText("Facebok Error");
+                Toast.makeText(getApplicationContext(), "Hubo un error al loguarte en la aplicacion", Toast.LENGTH_LONG);
+                //titulolbl.setText("Facebok Error");
             }
         });
     }
@@ -56,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+    private boolean estaLoguedo(){
+        return AccessToken.getCurrentAccessToken() != null;
+    }
 
 
 }
